b1f828521c3f64e95b9e1b51c48e77356793afde
mysk <wolfbot.owner@gmail.com>
2020-08-02 11:13:01 +0900

    feat(parser): implement parser

--- tmp.s
+++ tmp.s
@@ -1,7 +1,16 @@
 .intel_syntax noprefix
 .globl main
 main:
-  mov rax, 12
-  add rax, 34
-  sub rax, 5
+  push 12
+  push 34
+  push 5
+  pop rdi
+  pop rax
+  sub rax, rdi
+  push rax
+  pop rdi
+  pop rax
+  add rax, rdi
+  push rax
+  pop rax
   ret
